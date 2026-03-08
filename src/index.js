73e57e131210aee423391f176269cf50e8b83ea5
H.W.S.M.MUNASINGHE <2022t01534@stu.cmb.ac.lk>
2026-03-08 18:14:13 +0530

    Category feature added.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { default: Category } = require('./models/Category.js');
+const { default: categoryRouter } = require('./routes/categoryRouter.js');
 require('dotenv').config();
 
 const app = express();
 connectDB();
 
+//Middleware
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('API is running'));
+//Routes
+app.use('/api/categories', categoryRouter); 
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
