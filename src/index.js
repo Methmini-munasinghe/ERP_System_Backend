a4ee875b923e727cc963a15ad618d0fadf02452f
H.W.S.M.MUNASINGHE <2022t01534@stu.cmb.ac.lk>
2026-03-27 22:40:06 +0530

    made frontend connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const supplierRouter = require('./routes/supplierRouter.js');
 require('dotenv').config();
 
+
 const app = express();
 connectDB();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/suppliers', supplierRouter);
